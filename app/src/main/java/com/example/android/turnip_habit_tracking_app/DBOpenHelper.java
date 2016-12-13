9251cc42a19482d027212f5878750709842b4cce
Kevin <kevin.lee@movidius.com>
2016-12-13 16:25:50 +0000

    Added in SQLite Backend

---
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/DBOpenHelper.java
@@ -0,0 +1,46 @@
+package com.example.android.turnip_habit_tracking_app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    // constants for db name and version
+    private static final String DATABASE_NAME = "habits.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Constants for identifying table and columns
+    public static final String HABIT_NAME = "habitName";
+    public static final String HABIT_ID = "_id";
+    public static final String HABIT_START_DATE = "startDate";
+    public static final String HABIT_START_TIME = "startTime";
+    public static final String HABIT_CREATED = "habitCREATED";
+
+    //SQL to create Table
+    private  static final String TABLE_CREATE =  "CREATE TABLE " + HABIT_NAME + "(" +
+            HABIT_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            HABIT_START_DATE + " TEXT, " +
+            HABIT_START_TIME + " TEXT " +
+            HABIT_CREATED + " TEXT default CURRENT_TIMESTAMP" +
+            ")" ;
+
+
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXIST" + HABIT_NAME );
+        onCreate(db);
+    }
+
+
+}
--- app/src/main/java/com/example/android/turnip_habit_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.turnip_habit_tracking_app;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void insertHabit(String habitText){
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.HABIT_NAME, habitText);
+
     }
 }
