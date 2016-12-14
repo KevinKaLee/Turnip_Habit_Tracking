277338c845639d37d1e2ec7ff35714fe32050333
Kevin <kevin.lee@movidius.com>
2016-12-14 13:14:56 +0000

    Setted up ListView for main activity

---
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/HabitsProvider.java
@@ -0,0 +1,66 @@
+package com.example.android.turnip_habit_tracking_app;
+
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class HabitsProvider extends ContentProvider{
+
+    private static final String AUTHORITY = "com.example.android.turnip_habit_tracking_app.habitsprovider";
+    private static final String BASE_PATH = "habits";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+
+    // Constant to identify the requested operation
+    private static final int HABITS = 1;
+    private static final int HABITS_ID = 2;
+    private static  final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, HABITS);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", HABITS_ID);
+
+    }
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+        DBOpenHelper helper = new DBOpenHelper(getContext());
+        database = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String selection , String[] strings1, String s1) {
+        return database.query(DBOpenHelper.TABLE_HABITS, DBOpenHelper.ALL_COLUMNS,selection,null,null,null,DBOpenHelper.HABIT_CREATED + " DESC");
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        long id = database.insert(DBOpenHelper.TABLE_HABITS, null ,contentValues);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return database.delete(DBOpenHelper.TABLE_HABITS,selection,selectionArgs);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        return database.update(DBOpenHelper.TABLE_HABITS, contentValues , selection,selectionArgs);
+    }
+}
--- app/src/main/java/com/example/android/turnip_habit_tracking_app/MainActivity.java
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.android.turnip_habit_tracking_app;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        insertHabit();
+
+        Cursor cursor = getContentResolver().query(HabitsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+        String[] from = {DBOpenHelper.HABIT_NAME};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, 0);
+        ListView list = (ListView) findViewById(android.R.id.list);
+        list.setAdapter(cursorAdapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +55,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void insertHabit() {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.HABIT_NAME, "New Habit");
+        Uri habitURI = getContentResolver().insert(HabitsProvider.CONTENT_URI, values);
     }
 
     @Override
@@ -51,9 +88,45 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertHabit(String habitText){
+    private void insertHabit(String habitText) {
         ContentValues values = new ContentValues();
         values.put(DBOpenHelper.HABIT_NAME, habitText);
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
