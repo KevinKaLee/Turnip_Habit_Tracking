78f5cda36ac2b7fb4e9f8893ecfdc48031d942e4
Eoin Thompson <Eoin Thompson>
2016-12-20 14:55:15 +0000

    Alarm Manager integrated with app - bug with habit ID

---
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/Alarm.java
@@ -0,0 +1,13 @@
+package com.example.android.turnip_habit_tracking_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
---
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/AlarmTest.java
@@ -0,0 +1,13 @@
+package com.example.android.turnip_habit_tracking_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_test);
+    }
+}
