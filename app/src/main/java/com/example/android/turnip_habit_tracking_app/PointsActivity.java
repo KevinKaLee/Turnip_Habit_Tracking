becb5aba28c3d48769ad0186fc7602a956257370
emmafoley2 <emma.foley.2@ucdconnect.ie>
2016-12-23 02:06:10 +0000

    Added points total

--- app/src/main/java/com/example/android/turnip_habit_tracking_app/PointsActivity.java
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/PointsActivity.java
@@ -15,39 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.view.View;
 import android.widget.Toast;
 
-
-import android.content.ContentValues;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class PointsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private CursorAdapter cursorAdapter;
-    private String habitFilter;
-    private  Uri uri;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,19 +35,26 @@ public class PointsActivity extends AppCompatActivity implements LoaderManager.L
         list.setAdapter(cursorAdapter);
 
         TextView tv = (TextView) findViewById(R.id.PointsTotal);
-        int points = getTotal();//DBOpenHelper.getPointsTotal();
+        int points = getTotal();
         tv.setText(Integer.toString(points));
 
         getLoaderManager().initLoader(0, null, this);
-
-        Toast.makeText(getApplicationContext(),"Creating",Toast.LENGTH_SHORT).show();
     }
 
 
-    private void restartLoader() {
-        getLoaderManager().restartLoader(0, null, this);
-    }
+    public int getTotal() {
+        int total = 0;
+        Cursor cursor = getContentResolver().query(HabitsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null);
+        cursor.moveToFirst();
 
+        while (!cursor.isAfterLast()) {
+            // null could happen if we used our empty constructor
+            int points = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_POINTS));
+            total+=points;
+            cursor.moveToNext();
+        }
+        return total;
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -92,19 +72,6 @@ public class PointsActivity extends AppCompatActivity implements LoaderManager.L
     }
 
 
-    public int getTotal() {
-        int total = 0;
-        Cursor cursor = getContentResolver().query(HabitsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null);
-        cursor.moveToFirst();
-
-        while (!cursor.isAfterLast()) {
-            // null could happen if we used our empty constructor
-            int points = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_POINTS));
-            total+=points;
-            cursor.moveToNext();
-        }
-        return total;
-    }
 
 
 }
