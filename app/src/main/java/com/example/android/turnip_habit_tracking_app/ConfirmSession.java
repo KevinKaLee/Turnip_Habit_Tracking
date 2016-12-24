95f8ec0988e1f6551d49b150c9445d8bbedb45e4
emmafoley2 <emma.foley.2@ucdconnect.ie>
2016-12-24 00:50:36 +0000

    Refactored ConfirmSession methods

--- app/src/main/java/com/example/android/turnip_habit_tracking_app/ConfirmSession.java
+++ app/src/main/java/com/example/android/turnip_habit_tracking_app/ConfirmSession.java
@@ -27,7 +27,6 @@ public class ConfirmSession extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         final String habitID = bundle.getString("ID");
-        final String habitFilter = DBOpenHelper.HABIT_ID +  "=" + habitID;
 
         // Simple Dialog - Increment Reward if Yes is clicked??
         new AlertDialog.Builder(this)
@@ -35,26 +34,15 @@ public class ConfirmSession extends AppCompatActivity {
                 .setMessage("Have you completed the session?")
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+                        updateProgress(habitID);
 
-                        Cursor cursor = getContentResolver().query(HabitsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, habitFilter, null, null);
-                        cursor.moveToFirst();
-                        int points = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_POINTS));
-                        points +=5;
-                        int streak = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_STREAK));
-                        streak +=1;
-                        ContentValues values = new ContentValues();
-                        values.put(DBOpenHelper.HABIT_POINTS, points);
-                        values.put(DBOpenHelper.HABIT_STREAK, streak);
-                        getContentResolver().update(HabitsProvider.CONTENT_URI, values, habitFilter, null);
                     }
                 })
 
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(getApplication(),"Clicked - Yes",Toast.LENGTH_SHORT).show();
-                        ContentValues values = new ContentValues();
-                        values.put(DBOpenHelper.HABIT_STREAK, 0);
-                        getContentResolver().update(HabitsProvider.CONTENT_URI, values, habitFilter, null);
+                        resetStreak(habitID);
 
                     }
                 })
@@ -62,4 +50,35 @@ public class ConfirmSession extends AppCompatActivity {
                 .show();
     }
 
+    /**
+     * Updates progress for completed habit session. Points are incremented by 5 and streak by 1.
+     * @param habitID
+     */
+
+    private void updateProgress(String habitID){
+        String habitFilter = DBOpenHelper.HABIT_ID +  "=" + habitID;
+        Cursor cursor = getContentResolver().query(HabitsProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, habitFilter, null, null);
+        cursor.moveToFirst();
+        int points = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_POINTS));
+        points +=5;
+        int streak = cursor.getInt(cursor.getColumnIndex(DBOpenHelper.HABIT_STREAK));
+        streak +=1;
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.HABIT_POINTS, points);
+        values.put(DBOpenHelper.HABIT_STREAK, streak);
+        getContentResolver().update(HabitsProvider.CONTENT_URI, values, habitFilter, null);
+
+    }
+
+    /**
+     * Resets streak to zero for incomplete session. Points are unaffected.
+     * @param habitID
+     */
+
+    private void resetStreak(String habitID) {
+        String habitFilter = DBOpenHelper.HABIT_ID +  "=" + habitID;
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.HABIT_STREAK, 0);
+        getContentResolver().update(HabitsProvider.CONTENT_URI, values, habitFilter, null);
+    }
 }
